e749c2e1d307946f01a12497a9074330c3d7f906
unknown <manishmania20@gmail.com>
2025-09-07 02:25:26 +0530

    modified script

--- performance.sh
+++ performance.sh
@@ -2,33 +2,43 @@
 # Script to mesure the CPU, memory and disk usage of a remote server
 
 # Location to save the report
-REPORT_FILE="/var/log/system_performance_report.log"
+REPORT_FILE="/tmp/system_performance_report.txt"
 
-REPORT=$(cat <<EOF
---------------SYSTEM PERFORMANCE REPORT--------------
+print_header() {
+    echo "=============================="
+    echo "SYSTEM PERFORMANCE REPORT"
+    echo "Hostname: $(hostname)"
+    echo "Date: $(date)"
+    echo "=============================="
+    echo
+}
 
-Hostname: $(hostname)
-Date: $(date)
------------------------------------------------------
+cpu_usage () {
+    echo "CPU Usage:"
+    top -bn1 | grep "Cpu(s)" | awk '{print "Used: " 100 - $8 "% | Idle: " $8 "%"}'
+    echo "-----------------------------------------------------"
+    echo
+}
 
-CPU Usage:
-$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4 "%"}')
------------------------------------------------------
+memory_usage () {
+    echo "Memory Usage:"
+    free -h | awk '/Mem:/ {print "Used: " $3 " | Total: " $2}'
+    free -h | awk '/Mem:/ {print "Utilization: " $3/$2 * 100.0 "%"}'
+    echo "-----------------------------------------------------"
+    echo
+}
 
-Memory Usage:
-$(free -h | awk '/Mem:/ {print "Used: " $3 " | Total: " $2}')
-$(free -h | awk '/Mem:/ {print "Utilization: " $3/$2 * 100.0 "%"}')
------------------------------------------------------
+disk_usage () {
+    echo "Disk Usage:"
+    echo -e "Filesystem\tSize\tAvailable\tUse%"
+    df -h | grep -E '^/dev/' | awk '{print $1 "\t" $2 "\t" $4 "\t" $4/$2 * 100.0 "%"}'
+    echo "-----------------------------------------------------"
+    echo
+}
 
-Disk Usage:
-Filesystem      Total   Free    Available
-$(df -h | grep -E '^/dev/' | awk '{print $1 "\t" $2 "\t" $4 "\t" $4/$2 * 100.0 "%"}')
------------------------------------------------------
-EOF
-)
-
-# Output the report to the console
-echo "$REPORT"
-
-# Save the report to a file
-echo "$REPORT" >> $REPORT_FILE
+{
+    print_header
+    cpu_usage
+    memory_usage
+    disk_usage
+} | tee "$REPORT_FILE"
